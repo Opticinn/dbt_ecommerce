2f5c48f153bf5311e1a625d432764424a1ab27d4
Rafli <21523042@students.uii.ac.id>
2025-09-21 15:26:11 +0700

    Penambahan kolom age range pada dim_customers untuk dashboard Power BI

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -2,8 +2,9 @@ with base as (
   select
     customer_id,
     country,
-    age_min,
-    age_max,
+    age_range,
+    {{ age_bounds('age_range','min') }} as age_min,
+    {{ age_bounds('age_range','max') }} as age_max,
     signup_date
   from {{ ref('stg_customers') }}
 )
@@ -12,6 +13,7 @@ union all
 select
   -1::int         as customer_id,
   'Unknown'::text as country,
+  null::text       as age_range,
   null::int       as age_min,
   null::int       as age_max,
   null::date      as signup_date
